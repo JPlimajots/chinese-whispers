d39e00a96ff9338befff5ed0ed90c8b48397b5c3
JPlimajots <jplimaano@gmail.com>
2026-05-23 10:06:20 -0300

    feat: split dialogue into caller and receiver balloons using scalable ninepatchrect

--- scripts/dialogue_ui.gd
+++ scripts/dialogue_ui.gd
@@ -1,15 +1,21 @@
 extends CanvasLayer
 
-@onready var dialogue_label: RichTextLabel = $MainControl/BalloonBackground/DialogueLabel
+@onready var caller_balloon = $MainControl/CallerBalloon
+@onready var caller_label = $MainControl/CallerBalloon/CallerLabel
+@onready var receiver_balloon = $MainControl/ReceiverBalloon
+@onready var receiver_label = $MainControl/ReceiverBalloon/ReceiverLabel
 @onready var typewriter_timer: Timer = $TypewriterTimer
 #@onready var log_container: VBoxContainer = $LogContainer
 
 var current_text: String = ""
 var current_char_index: int = 0
-
+var active_label: RichTextLabel = null
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	caller_balloon.hide()
+	receiver_balloon.hide()
+	
 	ConnectionManager.connection_success.connect(_on_connection_success)
 	ConnectionManager.connection_chaos.connect(_on_connection_chaos)
 	ConnectionManager.connection_dropped.connect(_on_connection_dropped)
@@ -22,15 +28,15 @@ func _ready() -> void:
 func display_current_hook(call_id: String):
 	var call_data: Dictionary = NarrativeManager.NARRATIVES.get(call_id, {})
 	if not call_data.is_empty():
-		_start_typewriter(call_data["initial_text"])
+		_start_typewriter(call_data["initial_text"], caller_balloon, caller_label)
 	else:
-		_start_typewriter("STATIC_NOISE_LINE_TEXT")
+		_start_typewriter("STATIC_NOISE_LINE_TEXT", caller_balloon, caller_label)
 
 
 func _on_connection_success(caller_name: String, target_name: String):
 	var call_data: Dictionary = NarrativeManager.NARRATIVES.get(ConnectionManager.current_call_id, {})
 	if not call_data.is_empty():
-		_start_typewriter(call_data["success_text"])
+		_start_typewriter(call_data["success_text"], receiver_balloon, receiver_label)
 
 
 func _on_connection_chaos(caller_name: String, wrong_target_name: String, origin_id: String, target_id: String):
@@ -38,24 +44,26 @@ func _on_connection_chaos(caller_name: String, wrong_target_name: String, origin
 	var chaos_data: Dictionary = call_data.get("wrong_targets", {}).get(target_id, {})
 	
 	if not chaos_data.is_empty():
-		_start_typewriter(chaos_data["chaos_text"])
+		_start_typewriter(chaos_data["chaos_text"], receiver_balloon, receiver_label)
 
 
 func _on_connection_dropped(caller_name: String, reason: String):
-	_start_typewriter("LINE_MUTED: " + reason.to_upper())
+	_start_typewriter("LINE_MUTED: " + reason.to_upper(), receiver_balloon, receiver_label)
 
 
-func _start_typewriter(text_tag: String):
+func _start_typewriter(text_tag: String, target_balloon: Control, target_label: RichTextLabel):
 	ConnectionManager.is_typing = true
 	current_text = text_tag
-	dialogue_label.text = ""
 	current_char_index = 0
+	active_label = target_label
+	active_label.text = ""
+	target_balloon.show()
 	typewriter_timer.start()
 
 
 func _on_typewriter_timer_timeout():
-	if current_char_index < current_text.length():
-		dialogue_label.text += current_text[current_char_index]
+	if active_label and current_char_index < current_text.length():
+		active_label.text += current_text[current_char_index]
 		current_char_index += 1
 	else:
 		typewriter_timer.stop()
@@ -64,4 +72,7 @@ func _on_typewriter_timer_timeout():
 
 
 func _clear_text():
-	dialogue_label.text = ""
+	caller_label.text = ""
+	receiver_label.text = ""
+	caller_balloon.hide()
+	receiver_balloon.hide()
